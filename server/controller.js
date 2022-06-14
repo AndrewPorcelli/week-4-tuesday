8aa5e1c7c7afccdf83d2a9ea6d8cdf039515d2e7
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-06-14 15:09:00 -0600

    Finished the lab

---
+++ server/controller.js
@@ -0,0 +1,41 @@
+const houses = require("./db.json");
+
+const houseID = 4;
+
+module.exports = {
+  getHouses: (req, res) => {
+    res.status(200).send(houses);
+  },
+  deleteHouse: (req, res) => {
+    let index = houses.findIndex((elem) => elem.id === +req.params.id);
+    houses.splice(index, 1);
+    res.status(200).send(houses);
+  },
+  createHouse: (req, res) => {
+    const { address, price, imageURL } = req.body;
+    let newHouse = {
+      address,
+      price: +price,
+      imageURL,
+      id: houseID,
+    };
+    houses.push(newHouse);
+    res.status(200).send(houses);
+    houseID++;
+  },
+  updateHouse: (req, res) => {
+    const { type } = req.body;
+    let index = houses.findIndex((elem) => elem.id === +req.params.id);
+
+    console.log(type);
+    if (type === "minus") {
+      houses[index].price -= 10000;
+      res.status(200).send(houses);
+    } else if (type === "plus") {
+      houses[index].price += 10000;
+      res.status(200).send(houses);
+    } else {
+      res.status(400).send("Invalid star rating!");
+    }
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const {
+  getHouses,
+  createHouse,
+  updateHouse,
+  deleteHouse,
+} = require("./controller");
+
+// Endpoints ---------------
+app.get("/api/houses", getHouses);
+app.post("/api/houses", createHouse);
+app.put("/api/houses/:id", updateHouse);
+app.delete("/api/houses/:id", deleteHouse);
+
+const port = 4004;
+
+app.listen(port, console.log(`We are jiving on ${port}`));
